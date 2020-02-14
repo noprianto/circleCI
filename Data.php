1631d3ff9fe258f6feb2641a058dfa4cca2ac478
noprianto <noprianto1010@gmail.com>
2020-02-14 09:42:22 +0700

    Add files via upload

---
+++ Data.php
@@ -0,0 +1,8 @@
+<?php 
+$DatabaseType = 'mysqli'; 
+$DatabaseServer = 'localhost'; 
+$DatabaseUsername = 'super.user'; 
+$DatabasePassword = 'xaverian81'; 
+$DatabaseName = 'opensis'; 
+$DatabasePort = '3306'; 
+?>
